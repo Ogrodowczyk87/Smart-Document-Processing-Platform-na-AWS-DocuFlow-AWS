bb89f9cd2dbbea0ca1a3590e3b8bca944ea71355
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-05-23 10:16:37 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-950 px-6 text-white">
+      <h1 className="text-3xl font-semibold">DocuFlow AWS</h1>
+    </main>
+  );
+}
+
+export default App;
+
