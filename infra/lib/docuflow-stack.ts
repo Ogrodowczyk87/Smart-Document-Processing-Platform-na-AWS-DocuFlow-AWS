cf170a3e5bd0362a44966cf491bd1b19157d14dc
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-06-29 22:16:19 +0100

    secure documents S3 bucket

--- infra/lib/docuflow-stack.ts
+++ infra/lib/docuflow-stack.ts
@@ -1,4 +1,8 @@
-import { Stack, type StackProps } from "aws-cdk-lib";
+import {
+  RemovalPolicy,
+  Stack,
+  type StackProps,
+} from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
@@ -6,6 +10,19 @@ export class DocuFlowStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "DocumentsBucket");
+    new s3.Bucket(this, "DocumentsBucket", {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+      removalPolicy: RemovalPolicy.RETAIN,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.PUT],
+          allowedOrigins: ["http://localhost:5173"],
+          allowedHeaders: ["*"],
+          exposedHeaders: ["ETag"],
+        },
+      ],
+    });
   }
 }
