af920ce19743d6e469ddb380a964c99577ae1b36
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-06-30 22:01:57 +0100

    deploy presigned upload Lambda

--- infra/lib/docuflow-stack.ts
+++ infra/lib/docuflow-stack.ts
@@ -1,8 +1,12 @@
+import * as path from "node:path";
 import {
   RemovalPolicy,
   Stack,
   type StackProps,
 } from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
@@ -10,19 +14,59 @@ export class DocuFlowStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "DocumentsBucket", {
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      enforceSSL: true,
-      removalPolicy: RemovalPolicy.RETAIN,
-      cors: [
-        {
-          allowedMethods: [s3.HttpMethods.PUT],
-          allowedOrigins: ["http://localhost:5173"],
-          allowedHeaders: ["*"],
-          exposedHeaders: ["ETag"],
+    const documentsBucket = new s3.Bucket(
+      this,
+      "DocumentsBucket",
+      {
+        encryption: s3.BucketEncryption.S3_MANAGED,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        enforceSSL: true,
+        removalPolicy: RemovalPolicy.RETAIN,
+        cors: [
+          {
+            allowedMethods: [s3.HttpMethods.PUT],
+            allowedOrigins: ["http://localhost:5173"],
+            allowedHeaders: ["*"],
+            exposedHeaders: ["ETag"],
+          },
+        ],
+      },
+    );
+
+    const backendRoot = path.join(__dirname, "../../backend");
+
+    const createUploadUrlFunction = new NodejsFunction(
+      this,
+      "CreateUploadUrlFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_24_X,
+        entry: path.join(
+          backendRoot,
+          "src/handlers/createUploadUrl.ts",
+        ),
+        projectRoot: backendRoot,
+        depsLockFilePath: path.join(
+          backendRoot,
+          "package-lock.json",
+        ),
+        handler: "handler",
+        environment: {
+          DOCUMENTS_BUCKET_NAME: documentsBucket.bucketName,
         },
-      ],
-    });
+        bundling: {
+          bundleAwsSDK: true,
+          minify: true,
+          sourceMap: true,
+          target: "node24",
+        },
+      },
+    );
+
+    createUploadUrlFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:PutObject"],
+        resources: [documentsBucket.arnForObjects("documents/*")],
+      }),
+    );
   }
 }
